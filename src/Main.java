b0000ff336e0ffdbec1a5b9f7fee8a0124619532
maciejKorzeniowski <jakub.wolko@gmail.com>
2019-05-11 14:07:39 +0200

    tlumaczenie

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,11 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("witaj swiecie");
+
+
+        for (int i = 0; i <10; i++) {
+            System.out.println("liczba"+i);
+
+        }
     }
 }
